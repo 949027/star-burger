b7835ed7a577ee16ca3210c7347eecfd9ae8cf1f
949027 <82878668+949027@users.noreply.github.com>
2022-04-20 09:11:31 +0700

    Created new app

---
+++ location/models.py
@@ -0,0 +1,18 @@
+import datetime
+from django.db import models
+
+
+class Place(models.Model):
+    address = models.CharField('Адрес', max_length=200, unique=True)
+    lat = models.DecimalField('Широта', max_digits=8, decimal_places=6)
+    lon = models.DecimalField('Долгота', max_digits=8, decimal_places=6)
+    update_date = models.DateTimeField(
+        'Дата обновления',
+        default=datetime.datetime.now,
+    )
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+
