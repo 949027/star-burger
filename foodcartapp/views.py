b556195923c40af48682c333338b0d8d07354f48
949027 <82878668+949027@users.noreply.github.com>
2022-05-03 20:04:37 +0700

    Removed function

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -5,10 +5,10 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer, ListField
-import requests
 
 from .models import Product, Order, ProductItem
 from location.models import Place
+from location.utils import fetch_coordinates
 
 
 def banners_list_api(request):
@@ -77,24 +77,6 @@ class OrderSerializer(ModelSerializer):
         model = Order
         fields = ['id', 'firstname', 'lastname',
                   'phonenumber', 'address', 'products']
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
 
 
 @api_view(['POST'])
