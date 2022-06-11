9cda81ed9a426ad63946458e20257f3de791dce8
949027 <949027@gmail.com>
2022-06-11 08:29:39 +0000

    Token imported from .env

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd /opt/star-burger/
 source venv/bin/activate
 git pull
 pip install -r requirements.txt
@@ -8,5 +7,5 @@ npm ci --dev
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 systemctl restart star-burger.service
-curl -H "X-Rollbar-Access-Token: e2c138a51900498993cd390f86cc6a9a" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "product1", "revision":"'"$(git rev-parse HEAD)"'", "rollbar_name": "949027", "local_username": "user", "comment": "comment", "status": "succeeded"}'
+curl -H "X-Rollbar-Access-Token: "$(cat .env | grep ROLLBAR_TOKEN | cut -d "=" -f 2)"" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "product1", "revision":"'"$(git rev-parse HEAD)"'", "rollbar_name": "949027", "local_username": "user", "status": "succeeded"}'
 echo 'Deploy completed!'
