ddc91352e70a1bbcb078f0787bb18e7de0992115
949027 <82878668+949027@users.noreply.github.com>
2022-07-12 03:23:56 +0700

    Fixed request to Rollbar

--- docker_prod/deploy.sh
+++ docker_prod/deploy.sh
@@ -2,5 +2,5 @@
 git pull
 docker-compose up -d
 docker exec django python manage.py collectstatic --noinput
-curl -H "X-Rollbar-Access-Token: "$(cat .env.prod | grep ROLLBAR_TOKEN | cut -d "=" -f 2)"" -H "Content-Type: application/json" -X POST 'https://api.rollbar.>
+curl -H "X-Rollbar-Access-Token: "$(cat .env | grep ROLLBAR_TOKEN | cut -d "=" -f 2)"" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "product", "revision":"'"$(git rev-parse HEAD)"'", "status": "succeeded"}'
 echo 'Deploy completed!'
